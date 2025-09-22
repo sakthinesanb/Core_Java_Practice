adfa2b775897f19ed1dc479a4206730afd4a6c18
sakthinesanb <mail.sakthinesan@gmail.com>
2025-09-22 17:55:48 +0530

    second-commit-Added_new_line

--- src/org/javabasics/FirstProgram.java
+++ src/org/javabasics/FirstProgram.java
@@ -2,6 +2,7 @@ package org.javabasics;
 
 public class FirstProgram {
 public static void main(String[] args) {
+	System.out.println("First Java Program");
 	System.out.println("Hello world...");
 }
 }
