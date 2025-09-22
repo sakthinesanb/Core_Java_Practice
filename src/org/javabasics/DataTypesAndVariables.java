ad5f762684f4ba570bc7906d61fca7ba2f2000bb
sakthinesanb <mail.sakthinesan@gmail.com>
2025-09-22 17:52:00 +0530

    Fist-commit-CoreJavaPractice

---
+++ src/org/javabasics/DataTypesAndVariables.java
@@ -0,0 +1,9 @@
+package org.javabasics;
+
+public class DataTypesAndVariables {
+	
+public static void main(String[] args) {
+	String title="Datatypes and variables";
+	System.out.println(title);
+}
+}
