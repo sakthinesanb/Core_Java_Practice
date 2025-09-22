51ed749eca5e47ffbbc046dc629b898f2af9f875
sakthinesanb <mail.sakthinesan@gmail.com>
2025-09-22 18:20:59 +0530

    Third-commit-DataTypesAndVariables-Added-new-lines

--- src/org/javabasics/DataTypesAndVariables.java
+++ src/org/javabasics/DataTypesAndVariables.java
@@ -1,9 +1,35 @@
 package org.javabasics;
 
 public class DataTypesAndVariables {
+	int defaultValInt;
+	byte defaultValByte;
+	short defaultValShort;
+	long defaultValLong;
+	float defaultValFloat;
+	double defaultValDouble;
+	char defaultValChar;
+	boolean defaultValBoolean;
+	String defaultValString;
 	
+	private void defaultValOfDatatypes() {
+		System.out.println("\nDefault Value of datatypes:\n1.Premitive Datatypes:\n"
+				+"Int: "+defaultValInt
+				+"\nByte: "+defaultValByte
+				+"\nShort: "+defaultValShort
+				+"\nLong: "+defaultValLong
+				+"\nFloat: "+defaultValFloat
+				+"\nDouble: "+defaultValDouble
+				+"\nChar: "+defaultValChar
+				+"\nBoolean: "+defaultValBoolean
+				+"\n2.Non Premitive Datatypes:\n"
+				+"String: "+defaultValString
+				);
+
+	}
 public static void main(String[] args) {
-	String title="Datatypes and variables";
+	String title="Datatypes and variables:";
 	System.out.println(title);
+	DataTypesAndVariables obj=new DataTypesAndVariables();
+	obj.defaultValOfDatatypes();
 }
 }
