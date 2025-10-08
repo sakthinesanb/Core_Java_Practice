259e47d8f069a11877ea6065933339cef98f1e92
sakthinesanb <mail.sakthinesan@gmail>
2025-10-08 16:05:10 +0530

    added-second-line

--- src/org/javabasics/FirstProgram.java
+++ src/org/javabasics/FirstProgram.java
@@ -3,6 +3,7 @@ package org.javabasics;
 public class FirstProgram {
 public static void main(String[] args) {
 	System.out.println("First Java Program");
+	System.out.println("second Java Program");
 	System.out.println("Hello world...");
 }
 }
